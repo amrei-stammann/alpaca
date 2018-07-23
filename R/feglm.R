5c2e08b66db1ae059ed5b0bff8e81a617dd782de
amrei-stammann <Amrei.Stammann@hhu.de>
2018-07-23 19:44:17 +0200

    Fixed Example v0.2

--- R/feglm.R
+++ R/feglm.R
@@ -50,7 +50,7 @@
 #' data <- simGLM(1000L, 200L, 1805L, model = "logit")
 #' 
 #' # Fit 'feglm()'
-#' mod <- feglm(y ~ x1 + x2 + x3 | i + j, data)
+#' mod <- feglm(y ~ x1 + x2 + x3 | i + t, data)
 #' summary(mod)
 #' 
 #' # Recover estimates of fixed effects
